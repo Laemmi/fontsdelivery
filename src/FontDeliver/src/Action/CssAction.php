bb84e5c0a4abab3154d7bc6d5d3938fe916fe384
Michael Lämmlein <ml@spacerabbit.de>
2018-05-15 21:48:45 +0200

    Initial Commit

---
+++ src/FontDeliver/src/Action/CssAction.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace FontDeliver\Action;
+
+use Interop\Http\ServerMiddleware\DelegateInterface;
+use Interop\Http\ServerMiddleware\MiddlewareInterface as ServerMiddlewareInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\HtmlResponse;
+//use Zend\Diactoros\Response\JsonResponse;
+use Zend\Expressive\Router;
+use Zend\Expressive\Template;
+//use Zend\Expressive\Plates\PlatesRenderer;
+use Zend\Expressive\Twig\TwigRenderer;
+//use Zend\Expressive\ZendView\ZendViewRenderer;
+
+class CssAction implements ServerMiddlewareInterface
+{
+    private $router;
+
+    private $template;
+
+    public function __construct(Router\RouterInterface $router, Template\TemplateRendererInterface $template = null)
+    {
+        $this->router   = $router;
+        $this->template = $template;
+    }
+
+    public function process(ServerRequestInterface $request, DelegateInterface $delegate)
+    {
+        return new HtmlResponse($this->template->render('css::index', []));
+    }
+}
---
+++ src/FontDeliver/src/ConfigProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace FontDeliver;
+
+/**
+ * The configuration provider for the FontDeliver module
+ *
+ * @see https://docs.zendframework.com/zend-component-installer/
+ */
+class ConfigProvider
+{
+    /**
+     * Returns the configuration array
+     *
+     * To add a bit of a structure, each section is defined in a separate
+     * method which returns an array with its configuration.
+     *
+     * @return array
+     */
+    public function __invoke()
+    {
+        return [
+            'dependencies' => $this->getDependencies(),
+            'templates'    => $this->getTemplates(),
+        ];
+    }
+
+    /**
+     * Returns the container dependencies
+     *
+     * @return array
+     */
+    public function getDependencies()
+    {
+        return [
+            'invokables' => [
+            ],
+            'factories'  => [
+                Action\CssAction::class => Action\CssFactory::class,
+            ],
+        ];
+    }
+
+    /**
+     * Returns the templates configuration
+     *
+     * @return array
+     */
+    public function getTemplates()
+    {
+        return [
+            'paths' => [
+                'css'    => [__DIR__ . '/../templates/css'],
+//                'error'  => [__DIR__ . '/../templates/error'],
+//                'layout' => [__DIR__ . '/../templates/layout'],
+            ],
+        ];
+    }
+}
